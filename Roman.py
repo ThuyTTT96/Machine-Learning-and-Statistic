5367576fbe12f1e791a7911e2b4623ca38e52cc5
ThuyTTT96 <thuy.tran.2y@stu.hosei.ac.jp>
2021-08-30 11:05:23 +0900

    uploaded PCA

--- Roman.py
+++ Roman.py
@@ -1,40 +1,20 @@
-def romanToInt(s):
-    """
-    :type s: str
-    :rtype: int
-    """
-    
-    res = 0
-    RomanList = []
-    Roman = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C':100, 'D':500, 'M':1000,
-                'IV': 4, 'IX':9, 'XL': 40, 'XC':90, 'CD':400, 'CM':900}
-    SpecRoman = {'IV': 4, 'IX':9, 'XL': 40, 'XC':90, 'CD':400, 'CM':900}
-    if s in Roman.keys():
-        return Roman[s]
-    else:
-        characters = list(s)
-        print(characters)
-        i, j = 0, 1
-        while True:
-            if i < len(characters) -1:
-                if(characters[i]+characters[j]) in SpecRoman :
-                    RomanList.append(characters[i]+characters[j])
-                    i+=2
-                    j+=2
-                else:
-                    RomanList.append(characters[i])
-                    i+=1
-                    j+=1
-                    
-            elif i == len(characters) -1:
-                RomanList.append(characters[i])
-                break
-        print(RomanList)
-        for character in RomanList:
-            if character in Roman.keys():
-                res = res + Roman[character]
-        return res
+def romanToInt(s: str) -> int:
+    Roman= {"I":1, "V":2, "X":10, "L":50,"C":100, "D":500, "M":1000}
+    result = 0
+    i = 0
+    while i < len(s):
+        s1 =  Roman[s[i]]
+        if i + 1  < len(s):
+            s2  = Roman[s[i+1]]
+            if s1 >= s2:
+                result = result + s1   
+                i+=1
+            else:
+                result = result - s1
+                i+=1
+        else:
+            result = result + s1
+            i+=1
+    return result
 
-
-s= 'III'
-romanToInt(s)
+romanToInt("IV")
